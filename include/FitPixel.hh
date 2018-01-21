b0e45154bc9e020656831c9996e82af9669d73ab
wuhongyi <wuhongyi@pku.edu.cn>
2018-01-21 13:47:45 +0800

    20180121

--- include/FitPixel.hh
+++ include/FitPixel.hh
@@ -4,15 +4,18 @@
 // Author: Hongyi Wu(吴鸿毅)
 // Email: wuhongyi@qq.com 
 // Created: 六 1月 20 22:18:31 2018 (+0800)
-// Last-Updated: 日 1月 21 00:36:48 2018 (+0800)
+// Last-Updated: 日 1月 21 13:33:06 2018 (+0800)
 //           By: Hongyi Wu(吴鸿毅)
-//     Update #: 5
+//     Update #: 14
 // URL: http://wuhongyi.cn 
 
 #ifndef _FITPIXEL_H_
 #define _FITPIXEL_H_
 
+#include "TString.h"
+
 class TGraph;
+class TH1;
 class TH1I;
 class TH1D;
 class TH2I;
@@ -23,8 +26,17 @@ public:
   FitPixel(int n,double *x,double *y);
   virtual ~FitPixel();
 
+  // 用于区分归一类型，pixel的正背面信息等
+  void SetName(const char *name);
+  void SetName(const TString name);
+  
   void SetRobust(int rob = -1);// 0-100
-
+  void FitRobust();
+  TGraph *GetRobustFitGraph() { return orig; }
+  
+  void CalculateRobustFitEffectHistogram();
+  TH2I *GetRobustFitEffectTH2() { return h2_y_er; }
+  TH1I *GetRobustFitEffectTH1() { return h1_er; }
 
   
 public:
@@ -38,7 +50,10 @@ public:
   inline int GetPointN() { return pointn; }
 
 protected:
-  void FitRobust();
+  void GetRangeAbove(TH1 *h,double thre,int &left,int &right);
+  
+protected:
+  TString nameofsave;
 
 
   
@@ -51,7 +66,9 @@ private:
   double err1;
 
   TH2I *h2_y_er;
-  TH1D *h1_er;
+  TH1I *h1_er;
+  double h1_er_mean;
+  double h1_er_sigma;
   
 private:
   double *xx;
--- src/FitPixel.cc
+++ src/FitPixel.cc
@@ -4,15 +4,18 @@
 // Author: Hongyi Wu(吴鸿毅)
 // Email: wuhongyi@qq.com 
 // Created: 六 1月 20 22:19:14 2018 (+0800)
-// Last-Updated: 日 1月 21 00:41:47 2018 (+0800)
+// Last-Updated: 日 1月 21 13:46:09 2018 (+0800)
 //           By: Hongyi Wu(吴鸿毅)
-//     Update #: 12
+//     Update #: 24
 // URL: http://wuhongyi.cn 
 
 #include "FitPixel.hh"
 
 #include "TGraph.h"
 #include "TF1.h"
+#include "TH1.h"
+#include "TH2.h"
+#include "TString.h"
 
 #include <iostream>
 
@@ -26,14 +29,18 @@ FitPixel::FitPixel(int n,double *x,double *y)
   err0 = 0;
   err1 = 0;
   orig = NULL;
-
+  h2_y_er = NULL;
+  h1_er = NULL;
   
   pointn = n;
   xx = x;
   yy = y;
 
 
+
   orig = new TGraph(n,x,y);
+
+  
   // for (int i = 0; i < pointn; ++i)
   //   {
   //     std::cout<<i<<"  "<<x[i]<<"  "<<y[i]<<std::endl;
@@ -42,10 +49,22 @@ FitPixel::FitPixel(int n,double *x,double *y)
 
 FitPixel::~FitPixel()
 {
-  delete orig;
+  if(orig != NULL) delete orig;
+  if(h2_y_er != NULL) delete h2_y_er;
+  if(h1_er != NULL) delete h1_er;
 }
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void FitPixel::SetName(const char *name)
+{
+  nameofsave = TString(name);
+}
+
+void FitPixel::SetName(const TString name)  
+{
+  nameofsave = name;
+}
 
 void FitPixel::SetRobust(int rob)
 {
@@ -66,9 +85,55 @@ void FitPixel::FitRobust()
 
   par0 = orig->GetFunction("pol1")->GetParameter(0);
   par1 = orig->GetFunction("pol1")->GetParameter(1);
+
+  orig->SetName(TString::Format("orig_%s",nameofsave.Data()).Data());
+  orig->SetTitle(TString::Format("b_%f_k_%f",par0,par1).Data());
+
+  std::cout<<"FitRobust: "<<par0<<"  "<<par1<<std::endl;
+}
+
+void FitPixel::CalculateRobustFitEffectHistogram()
+{
+  h2_y_er = new TH2I(TString::Format("h2er_%s",nameofsave.Data()).Data(),"",200,-100,100,1000,0,8000);
+  h1_er = new TH1I(TString::Format("h1er_%s",nameofsave.Data()).Data(),"",1000,-50,50);
+  
+  for (int i = 0; i < pointn; ++i)
+    {
+      h2_y_er->Fill(yy[i]-(par1*xx[i]+par0),yy[i]);
+      h1_er->Fill(yy[i]-(par1*xx[i]+par0));
+    }
+  int left,right;
+  GetRangeAbove(h1_er,0.2,left,right);
+  h1_er->Fit("gaus","SQ","",left,right);
+
+  h1_er_mean = h1_er->GetFunction("gaus")->GetParameter(1);
+  h1_er_sigma = h1_er->GetFunction("gaus")->GetParameter(2);
+  
+  std::cout<<"h1 er: "<<h1_er_mean<<"  "<<h1_er_sigma<<std::endl;
+}
+
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void FitPixel::GetRangeAbove(TH1 *h,double thre,int &left,int &right)
+{
+  double maxbincontent = h->GetBinContent(h->GetMaximumBin());
+  left = h->FindFirstBinAbove(thre*maxbincontent);
+  right = h->FindLastBinAbove(thre*maxbincontent);
 }
 
 
 
 // 
 // FitPixel.cc ends here
+
+
+
+
+
+
+
+
+
+
