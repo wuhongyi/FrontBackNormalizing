6556ce54936c1809164c6ffdcbdc85a3f74404b5
wuhongyi <wuhongyi@pku.edu.cn>
2018-01-31 01:03:31 +0800

    20180131

--- main.cc
+++ main.cc
@@ -4,9 +4,9 @@
 // Author: Hongyi Wu(吴鸿毅)
 // Email: wuhongyi@qq.com 
 // Created: 五 1月 19 12:51:11 2018 (+0800)
-// Last-Updated: 二 1月 30 23:17:08 2018 (+0800)
+// Last-Updated: 三 1月 31 00:53:52 2018 (+0800)
 //           By: Hongyi Wu(吴鸿毅)
-//     Update #: 43
+//     Update #: 47
 // URL: http://wuhongyi.cn 
 
 #include "cailbration.hh"
@@ -84,28 +84,53 @@ int main(int argc, char *argv[])
   
   // SelectData *sd = new SelectData(16,16);
   // sd->ReservedUniformityData("data/le2_all.root","data/s_le2_all.root");
-
+  // sd->ReservedUniformityData("data/le2_sel_all.root","data/ss_le2_all.root");
+  
   //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
   
   // cailbration
   
   cailbration *cail = new cailbration(16,16);
-  // cail->SimpleCail("data/se2.root","testse2",8,false);
-  // cail->SimpleCail("data/se2.root","testse2",8,true);
-  // cail->GetSimpleCailPar("testse2",8,8);
-  // cail->TestSimpleCailEffect("data/se2.root","testse2","result_se2.root");
 
-  cail->SimpleCail("data/s_le2_all.root","test_le2",8,false,1);
-  cail->SimpleCail("data/s_le2_all.root","test_le2",8,true,1);
-  cail->GetSimpleCailPar("test_le2",8,8);
-  cail->TestSimpleCailEffect("data/s_le2_all.root","test_le2","resultsimple_le2_all.root");
-  cail->TestSimpleCailEffect("data/le2_all.root","test_le2","resultsimple_orig_le2_all.root");
+  // cail->SimpleCail("data/s_le2_all.root","test_le2",8,false,1);
+  // cail->SimpleCail("data/s_le2_all.root","test_le2",8,true,1);
+  // cail->GetSimpleCailPar("test_le2",8,8);
+  // cail->TestSimpleCailEffect("data/s_le2_all.root","test_le2","resultsimple_le2_all.root");
+  // cail->TestSimpleCailEffect("data/le2_all.root","test_le2","resultsimple_orig_le2_all.root");
+
+  // cail->SimpleCail("data/s_le2_all.root","testr_le2",8,false,0);
+  // cail->SimpleCail("data/s_le2_all.root","testr_le2",8,true,0);
+  // cail->GetSimpleCailPar("testr_le2",8,8);
+  // cail->TestSimpleCailEffect("data/s_le2_all.root","testr_le2","resultsimpler_le2_all.root");
+  // cail->TestSimpleCailEffect("data/le2_all.root","testr_le2","resultsimpler_orig_le2_all.root");
+
+  // cail->OverAllCail("data/s_le2_all.root","test_le2",false);
+  // cail->OverAllCail("data/s_le2_all.root","test_le2",true);
+  // cail->GetOverAllCailPar("test_le2");
+  // cail->TestOverAllCailEffect("data/s_le2_all.root","test_le2","resultoverall_le2_all.root");
+  // cail->TestOverAllCailEffect("data/le2_all.root","test_le2","resultoverall_orig_le2_all.root");
+
+
+
+  cail->SimpleCail("data/ss_le2_all.root","tests_le2",8,false,1);
+  cail->SimpleCail("data/ss_le2_all.root","tests_le2",8,true,1);
+  cail->GetSimpleCailPar("tests_le2",8,8);
+  cail->TestSimpleCailEffect("data/ss_le2_all.root","tests_le2","resultsimples_le2_all.root");
+  cail->TestSimpleCailEffect("data/le2_sel_all.root","tests_le2","resultsimples_orig_le2_all.root");
+
+  cail->SimpleCail("data/ss_le2_all.root","testsr_le2",8,false,0);
+  cail->SimpleCail("data/ss_le2_all.root","testsr_le2",8,true,0);
+  cail->GetSimpleCailPar("testsr_le2",8,8);
+  cail->TestSimpleCailEffect("data/ss_le2_all.root","testsr_le2","resultsimplesr_le2_all.root");
+  cail->TestSimpleCailEffect("data/le2_sel_all.root","testsr_le2","resultsimplesr_orig_le2_all.root");
   
-  cail->OverAllCail("data/s_le2_all.root","test_le2",false);
-  cail->OverAllCail("data/s_le2_all.root","test_le2",true);
-  cail->GetOverAllCailPar("test_le2");
-  cail->TestOverAllCailEffect("data/s_le2_all.root","test_le2","resultoverall_le2_all.root");
-  cail->TestOverAllCailEffect("data/le2_all.root","test_le2","resultoverall_orig_le2_all.root");
+  cail->OverAllCail("data/ss_le2_all.root","tests_le2",false);
+  cail->OverAllCail("data/ss_le2_all.root","tests_le2",true);
+  cail->GetOverAllCailPar("tests_le2");
+  cail->TestOverAllCailEffect("data/ss_le2_all.root","tests_le2","resultoveralls_le2_all.root");
+  cail->TestOverAllCailEffect("data/le2_sel_all.root","tests_le2","resultoveralls_orig_le2_all.root");
+
+
 
   
   // and enter the event loop...
